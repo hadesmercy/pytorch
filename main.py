032d431c5a4fb84d2eb57d69a2d2f83355a0fe32
Hades <hadesmercy@163.com>
2022-02-03 16:16:56 +0800

    📅

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-import torch
-x = torch.eye(8)
-print(x)
-# 以下代码只有在PyTorch GPU版本上才会执行
-if torch.cuda.is_available():
-    device = torch.device("cuda")          # GPU
-    y = torch.ones_like(x, device=device)  # 直接创建一个在GPU上的Tensor
-    x = x.to(device)                       # 等价于 .to("cuda")
-    z = x + y
-    print(z)
-    print(z.to("cpu", torch.double))       # to()还可以同时更改数据类型
+import cv2
+
+net = cv2.dnn.readNet(
+    "/Users/hadesmercy/PycharmProjects/pythonProject2/object_detection_crash_course/dnn_model/yolov4-tiny.cfg",
+    "/Users/hadesmercy/PycharmProjects/pythonProject2/object_detection_crash_course/dnn_model/yolov4-tiny.weights")
+cap = cv2.VideoCapture(0)
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(320, 320), scale=1 / 255)
+
+classes = []
+with open("/Users/hadesmercy/PycharmProjects/pythonProject2/object_detection_crash_course/dnn_model/classes.txt",
+          "r") as flie_object:
+    for class_name in flie_object.readlines():
+        class_name = class_name.strip()
+        classes.append(class_name)
+print(classes)
+
+while True:
+    ret, frame = cap.read()
+    (class_ids, score, bboxes) = model.detect(frame)
+    for class_ids, score, bbox in zip(class_ids, score, bboxes):
+        (x, y, w, h) = bbox
+        class_name = classes[class_ids]
+        print(x, y, w, h)
+        cv2.putText(frame, class_name, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, (200, 0, 50), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (200, 0, 50), 3)
+    print("clss_ids", class_ids)
+    print("scores", score)
+    print("bboxes", bboxes)
+
+    cv2.imshow("Frame", frame)
+    cv2.waitKey(1)
+cv2.namedWindow("Frame", 10)
+
+cv2.resizeWindow("Frame", 900, 700)
